1c55b3118c4a050147d11e29b87a39aa921020db
Sam <samandarusmanov1@gmail.com>
2023-05-20 21:13:03 +0500

    fix errors

--- src/index.js
+++ src/index.js
@@ -4,7 +4,10 @@ import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import { store } from "../src/shared/store";
-import { BrowserRouter as Router, HashRouter } from "react-router-dom";
+import {
+  //  BrowserRouter as Router,
+  HashRouter,
+} from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
